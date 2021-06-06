abcd2a28b2da6c5142c2489c4a937fd81904fb01
Goubault Virginie <virginie.goubault@gmail.com>
2021-06-06 16:22:06 +0200

    amélioration design

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,3 +1,29 @@
+/********************************* Texte qui défile ***********************************/
+
+const textAnim = document.querySelector("h1");
+
+new Typewriter(textAnim, {
+  deleteSpeed: 10,
+})
+  .changeDelay(70)
+  .typeString("Bonjour, je m'appelle Virginie,")
+  .pauseFor(300)
+  .typeString(" Dev Full-Stack JS.")
+  .pauseFor(1000)
+  .deleteChars(18)
+  .typeString("à la recherche d'un stage!")
+  .pauseFor(1000)
+  .deleteChars(26)
+  .typeString("curieuse, motivée et adaptable!")
+  .pauseFor(1000)
+  .deleteChars(31)
+  .typeString("je serais ravie de faire partie de votre équipe!")
+  .pauseFor(1000)
+  .deleteChars(48)
+  .typeString("n'hésitez pas à me contacter!")
+  .start();
+/********************************* Formulaire de contact **********************************/
+
 let submit = document.querySelector(".submit-button");
 let formInputs = document.querySelectorAll("form.contact-form > input");
 let message = document.querySelector(".message-form");
